7af778474a2b10a401da5bc9f769518b724d2d44
Bredding234 <Bredding@hartford.edu>
2020-12-17 10:54:07 -0500

    inital commit completed MyNavbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import MyNavbar from "./components/my-navbar/mynavbar.compenent";
+
+const App = () => {
+  return (
+    <div>
+        <MyNavbar />
+      My Portfolio
+    </div>
+  );
+}
+
+export default App;
